11ee45773d6b67c78f852604bff78c888d8b97e6
barlat <barlat@github.com>
2023-01-13 17:04:48 +0100

    temp

--- test/CarTest.cc
+++ test/CarTest.cc
@@ -2,6 +2,7 @@
 #include <vector>
 
 #include "../src/Car.h"
+#include "../src/World.h"
 /*
 Author: Bartosz Latosek
 Purpose: This is the file with tests for Car class
@@ -11,9 +12,25 @@ TEST(CarTest, ContructorTest) {
     std::vector<unsigned int> bodyRadiuses = {1, 1, 1, 1, 1, 1, 1, 1};
     std::vector<float> wheelRadiuses = {1, 1};
     
-    Car* car = new Car(bodyRadiuses, wheelRadiuses);
+    PCar car = std::make_unique<Car>(bodyRadiuses, wheelRadiuses);
     EXPECT_TRUE(car->getCarBody() != nullptr);
     EXPECT_TRUE(car->getLeftCircle() != nullptr);
     EXPECT_TRUE(car->getRightCircle() != nullptr);
-    delete car;
+}
+
+TEST(CarTest, CreateBodyTest) {
+    std::vector<unsigned int> bodyRadiuses = {1, 1, 1, 1, 1, 1, 1, 1};
+    std::vector<float> wheelRadiuses = {1, 1};
+    
+    World world;
+    PCar car = std::make_unique<Car>(bodyRadiuses, wheelRadiuses);
+    EXPECT_TRUE(car->getCarBody()->getBody() == nullptr);
+    EXPECT_TRUE(car->getLeftCircle()->getBody() == nullptr);
+    EXPECT_TRUE(car->getRightCircle()->getBody() == nullptr);
+    
+    car->createBody();
+
+    // EXPECT_TRUE(car->getCarBody()->getBody() != nullptr);
+    // EXPECT_TRUE(car->getLeftCircle()->getBody() != nullptr);
+    // EXPECT_TRUE(car->getRightCircle()->getBody() != nullptr);
 }
