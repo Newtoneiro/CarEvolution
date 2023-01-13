355ecd16c56070f411da41b70c30b2db9fb57355
Radosław Kostrzewski <01158754@pw.edu.pl>
2023-01-13 16:46:38 +0100

    Remove example test

--- test/ExampleTests.cc
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-/*
-Author: Bartosz Latosek
-Purpose: This is the file with example test for demonstration purposes
-*/
-TEST(Tests, DemonstrateGTestMacros) { EXPECT_TRUE(true); }
---
+++ test/WorldTest.cc
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+#include "../src/World.h"
+/*
+Autor: Radosław Kostrzewski
+Cel: Implementacja testów do klasy World
+*/
+
+
+TEST(WorldTest, ConstructorTest) {
+
+}
